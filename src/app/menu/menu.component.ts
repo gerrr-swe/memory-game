6444984d15542896b3428875937acf920f241668
gerrr-swe <gerardo.ramirez.swe@gmail.com>
2025-08-26 19:15:24 -0600

    feat: add menu style and structure

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../shared/button/button.component';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuPage {
-
+  cats() {
+    alert("cats!")
+  };
 }
