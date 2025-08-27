f1dc5fc1d77c67a122a22bcfe3b9a83e21efc494
gerrr-swe <gerardo.ramirez.swe@gmail.com>
2025-08-26 19:14:59 -0600

    feat: add button component

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.css'
 })
 export class ButtonComponent {
-
+  text = input("button")
 }
